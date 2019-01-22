e2f5c8e69e746667114e83302f9482adeae9b525
Cory Henderson <chenderson19899@gmail.com>
2019-01-21 18:59:49 -0800

    adds js logic

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ var imgId = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'ch
 var productImg1 = document.getElementById('productImg1');
 var productImg2 = document.getElementById('productImg2');
 var productImg3 = document.getElementById('productImg3');
-var container = document.getElementsByClassName('container');
 
 //Constructor to make new product objects and push to product array
 function BusProduct(name){
@@ -26,33 +25,44 @@ function buildProducts(){
 }
 buildProducts();
 
+
 //Generic random number generator
 function getRandomNum(){
-    return Math.floor(Math.random() * allProducts.length);
+    return (Math.floor(Math.random() * allProducts.length));
 }
 
-//Show (3) random, dissimilar products
+//Initialize dummy variables for duplicate checks
+var num1 = allProducts.length + 1;
+var num2 = allProducts.length + 1;
+var num3 = allProducts.length + 1;
+
+//Show (3) random, non duplicate products
 function showRandomProducts(){
     var randomNum = getRandomNum();
+    while(randomNum === num1 || randomNum === num2 || randomNum === num3){
+        var randomNum = getRandomNum();
+    }
+    num1 = randomNum;
     productImg1.src = allProducts[randomNum].filepath;
     productImg1.alt = allProducts[randomNum].name;
     productImg1.name = allProducts[randomNum].name;
     allProducts[randomNum].shows++;
-
+    
     var randomNum2 = getRandomNum();
-    while(randomNum2 === randomNum){
+    while(randomNum2 === randomNum || randomNum2 === num2 || randomNum2 === num3){
         randomNum2 = getRandomNum();
     }
-
+    num2 = randomNum2;
     productImg2.src = allProducts[randomNum2].filepath;
     productImg2.alt = allProducts[randomNum2].name;
     productImg2.name = allProducts[randomNum2].name;
     allProducts[randomNum2].shows++;
 
     var randomNum3 = getRandomNum();
-    while(randomNum3 === randomNum || randomNum3 === randomNum2){
+    while(randomNum3 === randomNum || randomNum3 === randomNum2 || randomNum3 === num3){
         randomNum3 = getRandomNum();
     }
+    num3 = randomNum3;
     productImg3.src = allProducts[randomNum3].filepath;
     productImg3.alt = allProducts[randomNum3].name;
     productImg3.name = allProducts[randomNum3].name;
@@ -60,13 +70,20 @@ function showRandomProducts(){
 }
 showRandomProducts();
 
-//Create event listener and handler function
-container.addEventListener('click', handleClick);
 
 function handleClick(event){
-    console.log(event.target);
+    var product = event.target.name;
+    console.log(event.target.name);
+    for(var i=0; i<allProducts.length; i++){
+        if(allProducts[i].name === product){
+            allProducts[i].clicks++;
+        }
+    }
     showRandomProducts();
 }
 
-
+//Create event listener and handler function
+productImg1.addEventListener('click', handleClick);
+productImg2.addEventListener('click', handleClick);
+productImg3.addEventListener('click', handleClick);
 
