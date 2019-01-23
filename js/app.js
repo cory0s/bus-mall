4d38ddaf3a29419b855f14d9ed0e86b808a4d6ad
Cory Henderson <chenderson19899@gmail.com>
2019-01-22 17:02:51 -0800

    adds chart

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 //global variables
 var allProducts = [];
@@ -7,6 +7,8 @@ var imgId = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'ch
 var productImg1 = document.getElementById('productImg1');
 var productImg2 = document.getElementById('productImg2');
 var productImg3 = document.getElementById('productImg3');
+var canvasChart = document.getElementById('chart');
+canvasChart.style.display = 'none';
 
 //Constructor to make new product objects and push to product array
 function BusProduct(name){
@@ -15,7 +17,7 @@ function BusProduct(name){
     this.shows = 0;
     this.clicks = 0;
     allProducts.push(this);
-};
+}
 
 //Build all products in img folder
 function buildProducts(){
@@ -38,39 +40,51 @@ var num3 = allProducts.length + 1;
 
 //Show (3) random, non duplicate products
 function showRandomProducts(){
+    // for(var i=0; i<3; i++){
+    //     var randomNum = getRandomNum();
+    //     if(duplicates.includes(randomNum) === false){
+    //         duplicates.push(randomNum);
+    //     } else {
+    //         randomNum = getRandomNum();
+    //         i--;
+    //     }
+    // }
+
     var randomNum = getRandomNum();
-    while(randomNum === num1 || randomNum === num2 || randomNum === num3){
-        var randomNum = getRandomNum();
+    if(randomNum === num1 || randomNum === num2 || randomNum === num3){
+        randomNum = getRandomNum();
     }
     num1 = randomNum;
-    productImg1.src = allProducts[randomNum].filepath;
-    productImg1.alt = allProducts[randomNum].name;
-    productImg1.name = allProducts[randomNum].name;
-    allProducts[randomNum].shows++;
-    
+    productImg1.src = allProducts[num1].filepath;
+    productImg1.alt = allProducts[num1].name;
+    productImg1.name = allProducts[num1].name;
+    allProducts[num1].shows++;
+
     var randomNum2 = getRandomNum();
-    while(randomNum2 === randomNum || randomNum2 === num2 || randomNum2 === num3){
+    if(randomNum2 === num1 || randomNum2 === num2 || randomNum2 === num3){
         randomNum2 = getRandomNum();
     }
     num2 = randomNum2;
-    productImg2.src = allProducts[randomNum2].filepath;
-    productImg2.alt = allProducts[randomNum2].name;
-    productImg2.name = allProducts[randomNum2].name;
-    allProducts[randomNum2].shows++;
+    productImg2.src = allProducts[num2].filepath;
+    productImg2.alt = allProducts[num2].name;
+    productImg2.name = allProducts[num2].name;
+    allProducts[num2].shows++;
 
     var randomNum3 = getRandomNum();
-    while(randomNum3 === randomNum || randomNum3 === randomNum2 || randomNum3 === num3){
+    if(randomNum3 === num1 || randomNum3 === num2 || randomNum3 === num3){
         randomNum3 = getRandomNum();
     }
     num3 = randomNum3;
-    productImg3.src = allProducts[randomNum3].filepath;
-    productImg3.alt = allProducts[randomNum3].name;
-    productImg3.name = allProducts[randomNum3].name;
-    allProducts[randomNum3].shows++;
+    productImg3.src = allProducts[num3].filepath;
+    productImg3.alt = allProducts[num3].name;
+    productImg3.name = allProducts[num3].name;
+    allProducts[num3].shows++;
 }
 showRandomProducts();
 
 
+//Create click handler function
+var totalClicks = 0;
 function handleClick(event){
     var product = event.target.name;
     console.log(event.target.name);
@@ -79,6 +93,17 @@ function handleClick(event){
             allProducts[i].clicks++;
         }
     }
+    totalClicks++;
+    if(totalClicks === 5){
+        productImg1.removeEventListener('click', handleClick);
+        productImg2.removeEventListener('click', handleClick);
+        productImg3.removeEventListener('click', handleClick);
+        totalClicks = 0;
+        canvasChart.style.display='block';
+    }
+
+    updateChart();
+    drawChart();
     showRandomProducts();
 }
 
@@ -87,3 +112,102 @@ productImg1.addEventListener('click', handleClick);
 productImg2.addEventListener('click', handleClick);
 productImg3.addEventListener('click', handleClick);
 
+
+function updateChart(){
+    for(var i=0; i<allProducts.length; i++){
+        clickArray[i]=allProducts[i].clicks;
+        productArray[i]=allProducts[i].name;
+    }
+}
+
+
+//Create chart data
+var clickArray=[];
+var productArray=[];
+
+//Data list for chart function input
+var data = {
+    labels: productArray, // titles array we declared earlier
+    datasets: [
+        {
+            data: clickArray, // votes array we declared earlier
+            backgroundColor: [
+                'bisque',
+                'darkgray',
+                'burlywood',
+                'lightblue',
+                'navy',
+                'bisque',
+                'darkgray',
+                'burlywood',
+                'lightblue',
+                'navy',
+                'bisque',
+                'darkgray',
+                'burlywood',
+                'lightblue',
+                'navy',
+                'navy',
+                'bisque',
+                'darkgray',
+                'burlywood',
+                'lightblue',
+                'navy',
+            ],
+            borderColor: [
+                'black'
+            ],
+            hoverBackgroundColor: [
+                'darkgreen',
+                'darkgreen',
+                'darkgreen',
+                'darkgreen',
+                'darkgreen'
+            ]
+        }]
+};
+
+
+//Create function to draw chart using options and data lists
+function drawChart(){
+    var ctx = document.getElementById('productChart').getContext('2d');
+    var productChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: data,
+
+        options: {
+            tooltips:{
+                xLabel: 'Number of Votes' 
+            },
+            legend:{
+                display: false
+            },
+            layout:{
+                padding:{
+                    top: 0,
+                    left: 100,
+                    right: 100,
+                    bottom: 25
+                }
+            },
+            title:{
+                display: true,
+                text: 'Product Popularity by Votes',
+                fontSize: 24
+            },
+            scales: {
+                xAxes: [{
+                    scaleLabel:{
+                        display: true,
+                        labelString: 'Number of Votes',
+                        fontSize: 14
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 1.0
+                    }
+                }]
+            }
+        }
+    });
+}
